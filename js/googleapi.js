f481cc8f5aed7eebade0f8859990042b5851c289
banmtG <banmtG@gmail.com>
2023-10-01 09:13:34 +0700

    a

--- js/googleapi.js
+++ js/googleapi.js
@@ -1,4 +1,4 @@
-let googleAppScript = ``https://script.google.com/macros/s/AKfycby98lPZ7LNGxk2I1zLNB9WsYJjgp4NRc68EFgCR1T5w6Lvs2YX0EyLOdjgDwnzIshuP_w/exec?getGeoCodeFromAddress=a``;
+let googleAppScript = `https://script.google.com/macros/s/AKfycby98lPZ7LNGxk2I1zLNB9WsYJjgp4NRc68EFgCR1T5w6Lvs2YX0EyLOdjgDwnzIshuP_w/exec?getGeoCodeFromAddress=a`;
 // let googleAppScript = `https://script.google.com/macros/s/AKfycbzm9n7EV1mTDckbu_Jy2xGqSpBpewHLvXYf0RDNo61JAYkHVA4-eS6rHQTULiVbx2d5yg/exec?myService=checkUser&name=AD&pass=BiTin2011`;
 
 fetch (googleAppScript)
