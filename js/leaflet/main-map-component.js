84eca852ce96724d102d60b1d8d0cf5eef62b717
banmtG <banmtG@gmail.com>
2023-10-05 19:13:11 +0700

    aa

--- js/leaflet/main-map-component.js
+++ js/leaflet/main-map-component.js
@@ -1,8 +1,8 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-var tiles =  L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+var tiles =  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    subdomains:['mt0']
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 map.addLayer(tiles);
